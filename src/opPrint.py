443248ce2aebeb104358040e2a8c97614654ae05
charles sutton <rapid537@zoho.com>
2022-10-16 23:27:55 -0400

    release prep

--- src/opPrint.py
+++ src/opPrint.py
@@ -2,12 +2,12 @@ import pprint
 import yaml
 
 
-def op(text_or_var, var=None, format=False, yml=False):
+def op(label_or_var, var=None, format=False, yml=False):
     """
     short-hand override print() formatter
 
     @params
-    text_or_var
+    label_or_var
     var=None
     format=False
     yml=False
@@ -15,15 +15,15 @@ def op(text_or_var, var=None, format=False, yml=False):
     try:
         if var and format:
             if yml:
-                return print('\n[', text_or_var, ']\n', yaml.dump(var))
-            return print('\n[', text_or_var, ']\n', pprint.pformat(var))
+                return print(f'\n{label_or_var}\r', yaml.dump(var))
+            return print(f'\n{label_or_var}\r', pprint.pformat(var))
         if not var and format:
             if yml:
-                return print('\n', yaml.dump(text_or_var))
-            return print('\n', pprint.pformat(text_or_var))
+                return print('\n', yaml.dump(label_or_var))
+            return print('\n', pprint.pformat(label_or_var))
         if var:
-            return print('\n[', text_or_var, ']\n', var, '\n')
-        return print('\n', text_or_var, '\n')
+            return print(f'\n{label_or_var}\n', var, '\n')
+        return print('\n', label_or_var, '\n')
     except Exception as error:
-        print('\n"op()" print override internal error...')
+        print('\nopPrint internal error...')
         print(error, '\n')
